ee3258f13405f4170b28f8fb33a939be98ed869b
SamuelKrabbe <krabbe.samuel@gmail.com>
2026-08-11 09:33:32 -0400

    first commit

---
+++ src/main/java/br/com/dominadores/xopxe/XopxeApplication.java
@@ -0,0 +1,13 @@
+package br.com.dominadores.xopxe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XopxeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XopxeApplication.class, args);
+	}
+
+}
